535ea3f0d6cb7d44fe6bf398df7a71f20b706da4
KhomsanKp <122007375+KhomsanKp@users.noreply.github.com>
2024-01-21 20:05:04 +0700

    delete key

--- env.d.ts
+++ env.d.ts
@@ -12,22 +12,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
     readonly env: ImportMetaEnv
 }
-
-// export default <KeyFirebase>{
-//     API_KEY:"AIzaSyCLXaMPI23VRi4Lqvy0R387--_K_PaXkVo",
-//     AUTH_DOMAIN:"stou-center.firebaseapp.com",
-//     PROJECT_ID:"stou-center",
-//     STORAGE_BUCKET:"stou-center.appspot.com",
-//     MESSAGING_SENDER_ID:"431824150719",
-//     APP_ID:"1:431824150719:web:d76e1acdb4ccdd5937e60a",
-// }
-
-// interface KeyFirebase {
-//     API_KEY:String,
-//     AUTH_DOMAIN:String,
-//     PROJECT_ID:String,
-//     STORAGE_BUCKET:String,
-//     MESSAGING_SENDER_ID:String,
-//     APP_ID:String,
-// }
-
